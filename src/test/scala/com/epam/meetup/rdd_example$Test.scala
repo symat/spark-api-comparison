16481178ef6011ba9ed88a541bafaad551cff64b
Mate Szalay-Beko <szalay.beko.mate@gmail.com>
2016-11-18 10:47:31 +0100

    add unit test skeleton

---
+++ src/test/scala/com/epam/meetup/rdd_example$Test.scala
@@ -0,0 +1,16 @@
+package com.epam.meetup
+
+import org.junit.Test
+import org.scalatest.FunSuite
+
+
+class rdd_example$Test extends FunSuite {
+
+  @Test
+  def shouldReturnCorrectScores() = {
+
+    rdd_example.main(Array("ee", "aa"))
+  }
+
+
+}
